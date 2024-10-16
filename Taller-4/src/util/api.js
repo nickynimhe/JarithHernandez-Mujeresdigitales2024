010b75e683d4cb8799abe1943f32e6282afa6432
Jarith Hernandez <nimhe000@gmail.com>
2024-10-15 22:49:05 -0500

    Agregar carpeta microblog-app dentro de Taller-5 la di toda ¿ok?

---
+++ Taller-4/src/util/api.js
@@ -0,0 +1,4 @@
+export const API_URL = "https://api.adviceslip.com/advice";
+// Esta línea exporta una constante llamada API_URL, que contiene la URL de una API externa.
+// La URL apunta a la API de Advice Slip, que proporciona consejos aleatorios.
+// La constante se exporta para que pueda ser importada y utilizada en otros archivos del proyecto.
