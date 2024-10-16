e52c16214bcde3c1110f0f1333c277210c2489e8
JarithHernandez <167656465+nickynimhe@users.noreply.github.com>
2024-10-15 22:38:25 -0500

    Delete src directory

--- src/api/usersApi.js
+++
@@ -1,7 +0,0 @@
-const API_URL = 'https://jsonplaceholder.typicode.com/users';
-
-export async function fetchUsers() {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  return data;
-}
