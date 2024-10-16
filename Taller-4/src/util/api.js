b37a8974fc7026a06fb11fa328196dc29fc3fb77
JarithHernandez <167656465+nickynimhe@users.noreply.github.com>
2024-10-15 22:55:26 -0500

    Delete Taller-4 directory

--- Taller-4/src/util/api.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = "https://api.adviceslip.com/advice";
-// Esta línea exporta una constante llamada API_URL, que contiene la URL de una API externa.
-// La URL apunta a la API de Advice Slip, que proporciona consejos aleatorios.
-// La constante se exporta para que pueda ser importada y utilizada en otros archivos del proyecto.
